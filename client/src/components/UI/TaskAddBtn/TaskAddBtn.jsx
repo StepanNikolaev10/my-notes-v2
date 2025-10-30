439bfc6edaedd92ff61b6120f8a685c24759aabe
NikolaS <nikolaevs1006@gmail.com>
2025-10-30 18:41:24 +0300

    feat: added component TaskAddBtn for TaskListPage

---
+++ client/src/components/UI/TaskAddBtn/TaskAddBtn.jsx
@@ -0,0 +1,11 @@
+import styles from './TaskAddBtn.module.scss'
+
+const TaskAddBtn = () => {
+  return (
+    <div className={styles.addBtn}>
+      <span></span>
+      <span></span>
+    </div>
+  )
+}
+export default TaskAddBtn
