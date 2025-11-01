63d42e2b3fd25fe411f9cc831013c5a0d38da8ef
NikolaS <nikolaevs1006@gmail.com>
2025-11-01 16:22:50 +0300

    style: update TaskItem component styles

--- client/src/components/Containers/TaskItem/TaskItem.jsx
+++ client/src/components/Containers/TaskItem/TaskItem.jsx
@@ -4,7 +4,7 @@ import styles from './TaskItem.module.scss';
 
 const TaskItem = ({ id, text, onTextChange }) => {
   return (
-    <div className={styles.TaskItem}>
+    <div className={styles.taskItem}>
       <div className={styles.taskContent}>
         <div className={styles.taskId}>{id}</div>
         <TaskInput
