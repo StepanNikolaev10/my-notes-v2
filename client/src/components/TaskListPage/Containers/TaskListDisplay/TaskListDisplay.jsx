ef438bf11a45b26379409bbbaeabccd7b13e1af9
NikolaS <nikolaevs1006@gmail.com>
2025-11-12 18:36:24 +0300

    feat: add text info in TaskListDisplay when task list is empty

--- client/src/components/TaskListPage/Containers/TaskListDisplay/TaskListDisplay.jsx
+++ client/src/components/TaskListPage/Containers/TaskListDisplay/TaskListDisplay.jsx
@@ -4,23 +4,26 @@ import styles from "./TaskListDisplay.module.scss";
 const TaskListDisplay = ({ tasks, onTextChange, onToggleTaskCheckbox, onEditTask, editableTasks }) => {
   return (
     <div className={styles.taskListDisplay}>
-      {tasks.map(task => {
-        const isEdit = editableTasks.some(editable => editable.id === task.id);
-
-        return (
-          <TaskItem
-            key={task.id}
-            id={task.id}
-            index={task.index}
-            text={task.text}
-            isCompleted={task.isCompleted}
-            onTextChange={onTextChange}
-            onToggleTaskCheckbox={onToggleTaskCheckbox}
-            onEditTask={onEditTask}
-            isEdit={isEdit}
-          />
-        );
-      })}
+      {tasks.length > 0 ? (
+        tasks.map(task => {
+          const isEdit = editableTasks.some(editable => editable.id === task.id);
+          return (
+            <TaskItem
+              key={task.id}
+              id={task.id}
+              index={task.index}
+              text={task.text}
+              isCompleted={task.isCompleted}
+              onTextChange={onTextChange}
+              onToggleTaskCheckbox={onToggleTaskCheckbox}
+              onEditTask={onEditTask}
+              isEdit={isEdit}
+            />
+          );
+        })
+      ) : (
+        <div className={styles.emptyListText}>Your task list is empty</div>
+      )}
     </div>
   );
 };
