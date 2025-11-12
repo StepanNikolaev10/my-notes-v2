f8b338e34d784592baafea499a0f8cf66a861e22
NikolaS <nikolaevs1006@gmail.com>
2025-11-12 23:22:06 +0300

    chore: update cross svg

--- client/src/components/Shared/Containers/AuthorizedHeader/EditTaskContent/EditTaskContent.jsx
+++ client/src/components/Shared/Containers/AuthorizedHeader/EditTaskContent/EditTaskContent.jsx
@@ -5,8 +5,8 @@ const EditTaskContent = ({ editableTasks, onDeleteTask }) => {
     <div className={styles.editTaskContent}>
       <div className={styles.colLeft}>
         <button className={styles.stopEditingBtn}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 1 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+          <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 20L4 4.00003M20 4L4.00002 20" stroke="gray" stroke-width="2" stroke-linecap="round"/>
           </svg>
         </button>
         <div className={styles.selectedTasksCount}>Выбрано: {editableTasks.length}</div>
