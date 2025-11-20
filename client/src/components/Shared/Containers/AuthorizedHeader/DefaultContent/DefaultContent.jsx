dad7d1ff807a0368573910c961b63977744411fc
NikolaS <nikolaevs1006@gmail.com>
2025-11-20 17:40:45 +0300

    refactor: update layout and refactor styles for it

--- client/src/components/Shared/Containers/AuthorizedHeader/DefaultContent/DefaultContent.jsx
+++ client/src/components/Shared/Containers/AuthorizedHeader/DefaultContent/DefaultContent.jsx
@@ -4,13 +4,9 @@ import styles from './DefaultContent.module.scss';
 const DefaultContent = () => {
   return (
     <div className={styles.defaultContent}>
-      <div className={styles.colLeft}>
-        <AppName/>
-      </div>
-      <div className={styles.colCenter}></div>
-      <div className={styles.colRight}>
-        
-      </div>
+      <AppName/>
+      <input className={styles.searchTaskInput} type="text"/>
+      <button className={styles.userNameBtn}>username</button>
     </div>
   )
 }
