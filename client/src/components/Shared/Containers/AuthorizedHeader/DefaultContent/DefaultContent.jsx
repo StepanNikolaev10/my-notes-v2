f3a4548fdf7994e1890d677c2b94df68204b42d5
NikolaS <nikolaevs1006@gmail.com>
2025-11-20 18:55:12 +0300

    feat: create new UserMenu component, update parent layout and its styles

--- client/src/components/Shared/Containers/AuthorizedHeader/DefaultContent/DefaultContent.jsx
+++ client/src/components/Shared/Containers/AuthorizedHeader/DefaultContent/DefaultContent.jsx
@@ -1,12 +1,13 @@
 import AppName from '/src/components/Shared/UI/AppName/AppName';
 import styles from './DefaultContent.module.scss';
+import UserMenu from './UserMenu/UserMenu';
 
 const DefaultContent = () => {
   return (
     <div className={styles.defaultContent}>
       <AppName/>
       <input className={styles.searchTaskInput} type="text"/>
-      <button className={styles.userNameBtn}>username</button>
+      <UserMenu/>
     </div>
   )
 }
---
+++ client/src/components/Shared/Containers/AuthorizedHeader/DefaultContent/UserMenu/UserMenu.jsx
@@ -0,0 +1,34 @@
+import { useState, useRef, useContext } from 'react';
+import styles from './UserMenu.module.scss';
+import { useClickOutside } from '../../../../../../hooks/useClickOutside';
+import { AuthContext } from '../../../../../../context';
+import { useNavigate } from 'react-router-dom';
+const UserMenu = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const menuRef = useRef(null);
+  const router = useNavigate();
+  const { setIsAuth } = useContext(AuthContext);
+
+  useClickOutside(menuRef, () => {
+    if (isDropdownOpen) setDropdownOpen(false);
+  });
+
+  const logout = () => {
+    localStorage.setItem('auth', 'false');
+    setIsAuth(false);
+    router(`/`);
+  }
+
+  return (
+    <div className={styles.userMenu} ref={menuRef}>
+      <button className={styles.userNameBtn} onClick={() => setDropdownOpen(!isDropdownOpen)}>username</button>
+      {isDropdownOpen && (
+        <div className={styles.dropdownMenu}>
+          <div className={styles.dropdownItem}>Settings</div>
+          <div className={styles.dropdownItem} onClick={logout}>Log out</div>
+        </div>
+      )}
+    </div>
+  )
+}
+export default UserMenu;
