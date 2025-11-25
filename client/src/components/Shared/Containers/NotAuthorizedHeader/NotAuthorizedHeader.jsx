62e6a5253fb7bcbefd31854ae43bb22923956e7e
NikolaS <nikolaevs1006@gmail.com>
2025-11-25 19:57:56 +0300

    refactor: change constant name

--- client/src/components/Shared/Containers/NotAuthorizedHeader/NotAuthorizedHeader.jsx
+++ client/src/components/Shared/Containers/NotAuthorizedHeader/NotAuthorizedHeader.jsx
@@ -1,4 +1,4 @@
-import { ACTIVE_PAGES_FOR_BTNS } from '../../../../constants/activePagesForBtns';
+import { AUTH_PAGE_VARIANTS } from '../../../../constants/authPageVariants';
 import AppName from '../../UI/AppName/AppName';
 import styles from './NotAuthorizedHeader.module.scss';
 import { Link } from 'react-router';
@@ -8,10 +8,10 @@ const NotAuthorizedHeader = ({ currentPage }) => {
     <header className={styles.notAuthorizedHeader}>
       <AppName/>
       <nav className={styles.authLinks}>
-        <Link to="/sign-in" className={`${styles.link} ${currentPage === ACTIVE_PAGES_FOR_BTNS.SIGN_IN ? styles.opened : ''}`}>
+        <Link to="/sign-in" className={`${styles.link} ${currentPage === AUTH_PAGE_VARIANTS .SIGN_IN ? styles.opened : ''}`}>
           Login
         </Link>
-        <Link to="/sign-up" className={`${styles.link} ${currentPage === ACTIVE_PAGES_FOR_BTNS.SIGN_UP ? styles.opened : ''}`}>
+        <Link to="/sign-up" className={`${styles.link} ${currentPage === AUTH_PAGE_VARIANTS .SIGN_UP ? styles.opened : ''}`}>
           Sign up
         </Link>
       </nav>
