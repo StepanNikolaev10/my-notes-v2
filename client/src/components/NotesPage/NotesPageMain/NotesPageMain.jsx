75456ae9b818208db923bf18d9fc3b88c7b1da3c
NikolaS <nikolaevs1006@gmail.com>
2025-11-28 18:22:57 +0300

    refactor: update styles and layout

--- client/src/components/NotesPage/NotesPageMain/NotesPageMain.jsx
+++ client/src/components/NotesPage/NotesPageMain/NotesPageMain.jsx
@@ -6,24 +6,27 @@ import { MODAL_VARIANTS } from "../../../constants/modalVariants";
 const NotesPageMain = ({ notes, onTextChange, onEditNotes, editableNotes, onOpenModal }) => {
   return (
     <div className={styles.notesPageMain}>
-      {notes.length > 0 ? (
-        notes.map(note => {
-          const isEdit = editableNotes.some(editable => editable.id === note.id);
-          return (
-            <NoteItem
-              key={note.id}
-              id={note.id}
-              content={note.content}
-              onTextChange={onTextChange}
-              onEditNotes={onEditNotes}
-              isEdit={isEdit}
-              noteStyles={note.styles}
-            />
-          );
-        })
-      ) : (
-        <div className={styles.emptyNotesText}>Your notes is empty</div>
-      )}
+      <div className={styles.notes}>
+        {notes.length > 0 ? (
+          notes.map(note => {
+            const isEdit = editableNotes.some(editable => editable.id === note.id);
+            return (
+              <NoteItem
+                key={note.id}
+                id={note.id}
+                content={note.content}
+                onTextChange={onTextChange}
+                onEditNotes={onEditNotes}
+                isEdit={isEdit}
+                noteStyles={note.styles}
+              />
+            );
+          })
+        ) : (
+          <div className={styles.emptyNotesText}>Your notes is empty</div>
+        )}
+      </div>
+
       <NoteAddBtn onClick={() => onOpenModal(MODAL_VARIANTS.NOTE_ADD)}/>
     </div>
   );
