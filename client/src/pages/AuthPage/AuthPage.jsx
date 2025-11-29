ef19b31f218f6c415b029c114f0fd1b21f8224d5
NikolaS <nikolaevs1006@gmail.com>
2025-11-29 12:30:43 +0300

    refactor: update renamed component paths and rename its in jsx

--- client/src/pages/AuthPage/AuthPage.jsx
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -1,11 +1,11 @@
-import NotAuthorizedHeader from '../../components/Shared/NotAuthorizedHeader/NotAuthorizedHeader';
 import styles from './AuthPage.module.scss';
 import AuthPageMain from '../../components/AuthPage/AuthPageMain/AuthPageMain';
+import NotAuthPagesHeader from '../../components/Shared/NotAuthPagesHeader/NotAuthPagesHeader';
 
 const AuthPage = ({ pageVariant }) => {
   return (
     <div className={styles.authPage}>
-      <NotAuthorizedHeader currentPage={pageVariant}/>
+      <NotAuthPagesHeader currentPage={pageVariant}/>
       <AuthPageMain pageVariant={pageVariant}/>
     </div>
   )
--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,11 @@
-import NotAuthorizedHeader from '../../components/Shared/NotAuthorizedHeader/NotAuthorizedHeader';
+import NotAuthPagesHeader from '../../components/Shared/NotAuthPagesHeader/NotAuthPagesHeader';
 import styles from './LandingPage.module.scss'; 
 import LandingPageMain from '../../components/LandingPage/LandingPageMain/LandingPageMain';
 
 const LandingPage = () => {
   return (
     <div className={styles.landingPage}>
-      <NotAuthorizedHeader/>
+      <NotAuthPagesHeader/>
       <LandingPageMain/>
     </div>
   );
