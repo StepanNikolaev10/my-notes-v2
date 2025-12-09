63929d9762d62854cecc0af6c47b26ba26975570
NikolaS <nikolaevs1006@gmail.com>
2025-12-09 22:26:53 +0300

    feat(constants): add new sort methods constant

---
+++ client/src/constants/notesSortMethods.js
@@ -0,0 +1,5 @@
+export const NOTES_SORT_METHODS = {
+  CUSTOM: { value: 'custom', label: 'Custom' },
+  DATE_CREATED: { value: 'date_created', label: 'Date created' },
+  DATE_MODIFIED: { value: 'date_modified', label: 'Date modified' }
+}
