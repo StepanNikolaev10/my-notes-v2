d6fc62eab438fe19852cee0765c53df81c32cf97
NikolaS <nikolaevs1006@gmail.com>
2025-12-09 23:28:53 +0300

    feat(NotesPage): add selected sort local state

--- client/src/pages/NotesPage/NotesPage.jsx
+++ client/src/pages/NotesPage/NotesPage.jsx
@@ -7,14 +7,25 @@ import AddNoteModal from '../../components/NotesPage/AddNoteModal/AddNoteModal';
 import { MODAL_CONTENT_VARIANTS } from '../../constants/modalContentVariants';
 import EditNoteColorModal from '../../components/Shared/EditNoteColorModal/EditNoteColorModal';
 import SelectSortModal from '../../components/NotesPage/SelectSortModal/SelectSortModal';
+import { NOTES_SORT_METHODS } from '../../constants/notesSortMethods';
 
 const NotesPage = () => {
   const notes = useNotesStore(state => state.notes);
 
   const [openedModal, setOpenedModal] = useState(null);
+  const [selectedSort, setSelectedSort] = useState(NOTES_SORT_METHODS.CUSTOM.value);
+
+  // const sortedNotes = useMemo(() => {
+
+  // }, selectedSort)
 
   const openModal = (type) => {
     setOpenedModal(type)
+  }
+
+  const sortNotes = (sort) => {
+    setSelectedSort(sort)
+    console.log(sort)
   }
 
   return (
@@ -39,6 +50,8 @@ const NotesPage = () => {
       {openedModal === MODAL_CONTENT_VARIANTS.SORT_SELECTING && (
         <SelectSortModal
           onClose={() => setOpenedModal(null)}
+          selectedSort={selectedSort}
+          onSelectSort={sortNotes}
         />
       )}
     </div>
