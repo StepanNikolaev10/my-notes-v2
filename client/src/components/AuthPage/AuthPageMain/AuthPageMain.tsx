25fba1072e18b37544de2e28d84a51002cc3582c
NikolaS <nikolaevs1006@gmail.com>
2026-01-04 19:59:42 +0300

    refactor(AuthPageMain): migrate component to ts

--- client/src/components/AuthPage/AuthPageMain/AuthPageMain.tsx
+++ client/src/components/AuthPage/AuthPageMain/AuthPageMain.tsx
@@ -1,7 +1,8 @@
 import AuthFormCard from '../AuthFormCard/AuthFormCard';
 import styles from './AuthPageMain.module.scss';
+import type { AuthPageVariants } from '../../../types/AuthPageTypes';
 
-const AuthPageMain = ({ pageVariant }) => {
+const AuthPageMain: React.FC<AuthPageVariants> = ({ pageVariant }) => {
     return (
       <main className={styles.authPageMain}>
         <AuthFormCard pageVariant={pageVariant}/>
