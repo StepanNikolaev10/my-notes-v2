ef666d4964aad385213e77bf427f42e039f117de
NikolaS <nikolaevs1006@gmail.com>
2026-01-04 18:56:24 +0300

    refactor(AuthPage): migrate AuthPage component to ts

--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -1,8 +1,9 @@
 import styles from './AuthPage.module.scss';
 import AuthPageMain from '../../components/AuthPage/AuthPageMain/AuthPageMain';
 import NotAuthPagesHeader from '../../components/Shared/NotAuthPagesHeader/NotAuthPagesHeader';
+import type { AuthPageVariants } from '../../types/AuthPageTypes';
 
-const AuthPage = ({ pageVariant }) => {
+const AuthPage: React.FC<AuthPageVariants> = ({ pageVariant }) => {
   return (
     <div className={styles.authPage}>
       <NotAuthPagesHeader currentPage={pageVariant}/>
