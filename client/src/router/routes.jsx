e4d70f80e15c7277b05e9a444542a1d5000ca6e8
NikolaS <nikolaevs1006@gmail.com>
2026-01-04 19:06:16 +0300

    refactor: remove usage of global AuthPageVariants constant in routes

--- client/src/router/routes.jsx
+++ client/src/router/routes.jsx
@@ -2,7 +2,6 @@ import LandingPage from "../pages/LandingPage/LandingPage";
 import NotePage from "../pages/NotePage/NotePage";
 import NotesPage from "../pages/NotesPage/NotesPage";
 import AuthPage from "../pages/AuthPage/AuthPage";
-import { AUTH_PAGE_VARIANTS } from "../constants/authPageVariants";
 import NotesSearchPage from "../pages/NotesSearchPage/NotesSearchPage";
 
 export const privateRoutes = [
@@ -13,6 +12,6 @@ export const privateRoutes = [
 
 export const publicRoutes = [
     {path: '/', element: <LandingPage/>, exact: true},
-    {path: '/sign-in', element: <AuthPage pageVariant={AUTH_PAGE_VARIANTS.SIGN_IN}/>, exact: true},
-    {path: '/sign-up', element: <AuthPage pageVariant={AUTH_PAGE_VARIANTS.SIGN_UP}/>, exact: true},
+    {path: '/sign-in', element: <AuthPage pageVariant={'SIGN_IN'}/>, exact: true},
+    {path: '/sign-up', element: <AuthPage pageVariant={'SIGN_UP'}/>, exact: true},
 ]
