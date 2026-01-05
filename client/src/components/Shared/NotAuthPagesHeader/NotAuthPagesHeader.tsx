ddeaa488912cc4c7b55ae65f6af6a721ca00a6a0
NikolaS <nikolaevs1006@gmail.com>
2026-01-05 17:43:43 +0300

    refactor(NotAuthPagesHeader): migrate component to ts

---
+++ client/src/components/Shared/NotAuthPagesHeader/NotAuthPagesHeader.tsx
@@ -0,0 +1,22 @@
+import AppName from '../UI/AppName/AppName';
+import Header from '../UI/Header/Header';
+import styles from './NotAuthPagesHeader.module.scss';
+import { Link } from 'react-router-dom';
+import type NotAuthPagesVariants from '../../../types/NotAuthPagesVariants';
+
+const NotAuthPagesHeader:React.FC<NotAuthPagesVariants> = ({ pageVariant }) => {
+  return (
+    <Header>
+      <AppName/>
+      <nav className={styles.authLinks}>
+        <Link to="/sign-in" className={`${styles.link} ${pageVariant === 'SIGN_IN' ? styles.opened : ''}`}>
+          Login
+        </Link>
+        <Link to="/sign-up" className={`${styles.link} ${pageVariant === 'SIGN_UP' ? styles.opened : ''}`}>
+          Sign up
+        </Link>
+      </nav>
+    </Header>
+  )
+}
+export default NotAuthPagesHeader;
