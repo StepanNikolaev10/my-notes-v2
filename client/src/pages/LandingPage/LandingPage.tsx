f229798a1e426056374e4ed2a93806a5d1c5faf5
NikolaS <nikolaevs1006@gmail.com>
2026-01-05 17:46:49 +0300

    refactor(LandingPage): migrate component to ts

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,11 +1,13 @@
 import NotAuthPagesHeader from '../../components/Shared/NotAuthPagesHeader/NotAuthPagesHeader';
 import styles from './LandingPage.module.scss'; 
 import LandingPageMain from '../../components/LandingPage/LandingPageMain/LandingPageMain';
+import type NotAuthPagesVariants from '../../types/NotAuthPagesVariants';
 
-const LandingPage = () => {
+const LandingPage:React.FC<NotAuthPagesVariants> = ({ pageVariant }) => {
+
   return (
     <div className={styles.landingPage}>
-      <NotAuthPagesHeader/>
+      <NotAuthPagesHeader pageVariant={pageVariant}/>
       <LandingPageMain/>
     </div>
   );
