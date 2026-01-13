b9cd2b72baed7f2b35ce6dd983b83d4e706b91d4
NikolaS <nikolaevs1006@gmail.com>
2026-01-13 14:21:04 +0300

    feat(pages): add TrashPage component

---
+++ client/src/pages/TrashPage/TrashPage.tsx
@@ -0,0 +1,11 @@
+import styles from './TrashPage.module.scss';
+import MainHeader from '../../components/Shared/MainHeader/MainHeader';
+
+const TrashPage = () => {
+  return (
+    <div className={styles.trashPage}>
+      
+    </div>
+  )
+}
+export default TrashPage;
--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -3,15 +3,17 @@ import NotePage from "../pages/NotePage/NotePage";
 import NotesPage from "../pages/NotesPage/NotesPage";
 import AuthPage from "../pages/AuthPage/AuthPage";
 import NotesSearchPage from "../pages/NotesSearchPage/NotesSearchPage";
+import TrashPage from "../pages/TrashPage/TrashPage";
 
 export const privateRoutes = [
-    {path: '/', element: <NotesPage/>, exact: true},
-    {path: '/notes/:id', element: <NotePage/>, exact: true},
-    {path: '/search', element: <NotesSearchPage/>, exact: true}
+    {path: '/', element: <NotesPage/>},
+    {path: '/notes/:id', element: <NotePage/>},
+    {path: '/search', element: <NotesSearchPage/>},
+    {path: '/trash', element: <TrashPage/>}
 ]
 
 export const publicRoutes = [
-    {path: '/', element: <LandingPage pageVariant={'LANDING'}/>, exact: true},
-    {path: '/sign-in', element: <AuthPage pageVariant={'SIGN_IN'}/>, exact: true},
-    {path: '/sign-up', element: <AuthPage pageVariant={'SIGN_UP'}/>, exact: true},
+    {path: '/', element: <LandingPage pageVariant={'LANDING'}/>},
+    {path: '/sign-in', element: <AuthPage pageVariant={'SIGN_IN'}/>},
+    {path: '/sign-up', element: <AuthPage pageVariant={'SIGN_UP'}/>},
 ]
