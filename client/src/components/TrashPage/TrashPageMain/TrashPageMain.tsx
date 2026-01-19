0ce7fd54dc8b22d0a4931872760737649923d690
NikolaS <nikolaevs1006@gmail.com>
2026-01-19 15:45:59 +0300

    chore(TrashPageMain): rename parent className

--- client/src/components/TrashPage/TrashPageMain/TrashPageMain.tsx
+++ client/src/components/TrashPage/TrashPageMain/TrashPageMain.tsx
@@ -5,12 +5,11 @@ import useNotesStore from '../../../store/useNotesStore';
 const TrashPageMain = () => {
   const trashedNotes = useNotesStore(state => state.trashedNotes);
   return (
-    <main className={styles.trashPageMain}>
+    <main className={styles.main}>
       {trashedNotes.length > 0
         ? <NotesList notes={trashedNotes}/>
         : <div className={styles.noNotesMessage}>No notes in trash</div>
       }
-
     </main>
   )
 }
