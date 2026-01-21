751ee6f6f74bdae840d371e5f11f32ac21288d4b
NikolaS <nikolaevs1006@gmail.com>
2026-01-21 19:40:46 +0300

    refactor(constants): make NOTES_SORT_METHODS readonly constant with key type and values object

--- client/src/constants/notesSortMethods.ts
+++ client/src/constants/notesSortMethods.ts
@@ -1,5 +1,9 @@
 export const NOTES_SORT_METHODS = {
-  CUSTOM: { value: 'custom', label: 'Custom' },
-  DATE_CREATED: { value: 'date_created', label: 'Date created' },
-  DATE_MODIFIED: { value: 'date_modified', label: 'Date modified' }
-}
+  CUSTOM: { value: 'CUSTOM', label: 'Custom' },
+  DATE_CREATED: { value: 'DATE_CREATED', label: 'Date created' },
+  DATE_MODIFIED: { value: 'DATE_MODIFIED', label: 'Date modified' }
+} as const
+
+export type NotesSortMethodsKeysType = keyof typeof NOTES_SORT_METHODS;
+
+export const notesSortMethodsList = Object.values(NOTES_SORT_METHODS);
