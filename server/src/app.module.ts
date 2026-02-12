7bca09be5d1dc3a641a9e7ebfb54750ac4198ad8
NikolaS <nikolaevs1006@gmail.com>
2026-02-12 19:27:26 +0300

    feat(app): add database connection configuration to AppModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: []
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', // тип базы данных
+      host: 'postgres', // хост
+      port: 5432, // стандартный порт PostgreSQL
+      username: 'postgres', // ваше имя пользователя в БД
+      password: 'password', // пароль от БД
+      database: 'postgres-db', // имя БД
+      autoLoadEntities: true, // автоматически подключает все entities
+      synchronize: true, // авто-создание таблиц по entity
+    }),
+    AuthModule
+  ] 
 })
 export class AppModule {}
