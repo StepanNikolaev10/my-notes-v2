5390a9de64efff37b170e3242626c136ba897ece
NikolaS <nikolaevs1006@gmail.com>
2026-02-14 23:44:21 +0300

    feat(user): sync UserCreateDto types with IUser interface

--- server/src/users/dto/user-create.dto.ts
+++ server/src/users/dto/user-create.dto.ts
@@ -1,5 +1,7 @@
-export class UserCreateDto {
-  readonly email: string;
-  readonly username: string;
-  readonly password: string;
+import type { IUser } from '../../../../shared/interfaces/user.interface';
+
+export class UserCreateDto { // по сути тип, но для использования декораторов это класс
+  readonly email: IUser['email'];
+  readonly username: IUser['username'];
+  readonly password: IUser['password'];
 }
