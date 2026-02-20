6ac9fffec09e8fbc5ab1ea2522b341754d1e3858
NikolaS <nikolaevs1006@gmail.com>
2026-02-20 22:26:47 +0300

    feat(auth): add auth response dto for return types in methods

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserCreateDto } from 'src/users/dto/user-create.dto';
 import { UserLoginDto } from './dto/user-login.dto';
-
+import { AuthResponseDto } from './dto/auth-response.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,13 +10,13 @@ export class AuthController {
   constructor( private authService: AuthService) {}
   
   @Post('/registration')
-  registration(@Body() dto: UserCreateDto) {
-    return this.authService.registration(dto)
+  registration(@Body() dto: UserCreateDto): Promise<AuthResponseDto> {
+    return this.authService.registration(dto);
   }
 
   @Post('/login')
-  login(@Body() dto: UserLoginDto) {
-    return this.authService.login(dto)
+  login(@Body() dto: UserLoginDto): Promise<AuthResponseDto> {
+    return this.authService.login(dto);
   }
 
 }
---
+++ server/src/auth/dto/auth-response.dto.ts
@@ -0,0 +1,4 @@
+export class AuthResponseDto {
+  readonly accessToken: string;
+  readonly refreshToken: string;
+}
