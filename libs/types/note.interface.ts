b1e77912cc5a7d02d8f3d826d7553097da6e0335
NikolaS <nikolaevs1006@gmail.com>
2026-03-12 20:38:47 +0300

    refactor: rename User and Note interfaces to names without I prefix

--- libs/types/note.interface.ts
+++ libs/types/note.interface.ts
@@ -1,10 +1,9 @@
-import type { NoteColorsKeysType } from "../../shared/constants/note-colors"
-
-export interface INote {
+import type { NoteColors } from "./note-colors.interface";
+export interface Note {
   id: number;
   title: string,
   mainText: string
-  colorKey: NoteColorsKeysType;
+  colorKey: keyof NoteColors;
   createdAt: Date | string; // на клиент эти данные приходят строкой из за JSON.stringify
   updatedAt: Date | string; // на клиент эти данные приходят строкой из за JSON.stringify
 }
