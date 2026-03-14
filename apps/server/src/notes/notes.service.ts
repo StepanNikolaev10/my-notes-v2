4f8bfd76c3fbc4f94c1b0e0ec82b804f6c0d560b
NikolaS <nikolaevs1006@gmail.com>
2026-03-14 18:16:44 +0300

    refactor(notes): update imports name in notes service

--- apps/server/src/notes/notes.service.ts
+++ apps/server/src/notes/notes.service.ts
@@ -1,20 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { AddNoteDto } from './dto/add-note.dto';
-import { RemoveNoteDto } from './dto/remove-note.dto';
-import { Note } from './note.entity';
+import { AddNoteDto } from './dto/req/add-note.dto';
+import { RemoveNoteDto } from './dto/req/remove-note.dto';
+import { NoteEntity } from './note.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import type { IUser } from '../../../../shared/interfaces/user.interface';
+import { User } from '@my-notes/types';
 
 @Injectable()
 export class NotesService {
 
-  @InjectRepository(Note) // даёт репозиторий(пульт управления на таблицей) для свойства ниже, ...
+  @InjectRepository(NoteEntity) // даёт репозиторий(пульт управления на таблицей) для свойства ниже, ...
   // ...свойство не будет undefined из за присваивания значения декоратором.
-  private noteRepository: Repository<Note>  // значения падает в userRepository,  ...
+  private noteRepository: Repository<NoteEntity>  // значения падает в userRepository,  ...
   // ...но для удобной работы используется тип Repository с добавлением методов автокомлитом и дженерик с типами сущности Note
 
-  async addNote(dto: AddNoteDto, userId: IUser['id']): Promise<Note> { // за счёт noteRepository создаётся экземпляр класса сущности Note, значения из dto подставляются в нужные места.
+  async addNote(dto: AddNoteDto, userId: User['id']): Promise<NoteEntity> { // за счёт noteRepository создаётся экземпляр класса сущности Note, значения из dto подставляются в нужные места.
     const note = this.noteRepository.create({
       ...dto,
       authorId: userId,
