c1fb54d4ec97fc3dec28f51520c6d39279e01467
NikolaS <nikolaevs1006@gmail.com>
2026-03-16 19:13:25 +0300

    feat(auth): update registration, login, refresh logic in auth service

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,57 +1,101 @@
-import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
-import { UserCreateDto } from 'src/users/dto/user-create.dto';
-import { UserLoginDto } from './dto/user-login.dto';
+import { HttpException, HttpStatus, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-import { JwtService } from '@nestjs/jwt';
-import { User } from 'src/users/user.entity';
+import { JsonWebTokenError, JwtService } from '@nestjs/jwt';
 import bcrypt from "bcryptjs";
 import { v4 as uuidv4 } from 'uuid';
-import { ConfigService } from '@nestjs/config';
-import { AuthResponseDto } from './dto/auth-response.dto';
+import { SessionsService } from 'src/sessions/sessions.service';
+import { TokensService } from 'src/tokens/tokens.service';
+import { UserRegistrationDto } from './dto/req/user-registration.dto';
+import { UserLoginDto } from './dto/req/user-login.dto';
+import { UpdateSession } from 'src/sessions/interfaces/update-session.interface';
+import { User } from '@my-notes/types';
+import type { RefreshTokenPayload } from 'src/tokens/interfaces/tokens-payload.interface';
+import { AuthServiceRes } from './interfaces/AuthServiceRes.interface';
 
 @Injectable()
 export class AuthService {
 
   constructor(
-    private usersService: UsersService,
-    private jwtService: JwtService,
-    private configService: ConfigService
+    private readonly usersService: UsersService,
+    private readonly sessionsService: SessionsService,
+    private readonly tokensService: TokensService,
+    private readonly jwtService: JwtService
   ) {}
 
-  async registration(dto: UserCreateDto): Promise<AuthResponseDto> {
+  async registration(dto: UserRegistrationDto): Promise<AuthServiceRes> {
     const candidate = await this.usersService.getUserByEmail(dto.email);
     if(candidate) {
-      throw new HttpException('Пользователь с таким email существует', HttpStatus.BAD_REQUEST)
+      throw new HttpException('User with this email already exists', HttpStatus.BAD_REQUEST)
     }
+    
     const hashPassword = await bcrypt.hash(dto.password, 5)
     const user = await this.usersService.createUser({...dto, password: hashPassword})
-    return this.generateJwts(user)
+
+    const jwts = {
+      accessToken: await this.tokensService.generateAccessToken(user.id),
+      refreshToken: await this.tokensService.generateRefreshToken(user.id, uuidv4())
+    }
+    const refreshPayload = this.jwtService.decode(jwts.refreshToken);
+
+    await this.sessionsService.createSession(refreshPayload)
+    return jwts;
   }
 
-  async login(dto: UserLoginDto): Promise<AuthResponseDto> {
-    const user = await this.validateUser(dto)
-    return this.generateJwts(user)
+  async login(dto: UserLoginDto): Promise<AuthServiceRes> {
+    const user = await this.validateUser(dto);
+
+    const jwts = {
+      accessToken: await this.tokensService.generateAccessToken(user.id),
+      refreshToken: await this.tokensService.generateRefreshToken(user.id, uuidv4())
+    }
+    const refreshPayload = this.jwtService.decode(jwts.refreshToken);
+
+    await this.sessionsService.createSession(refreshPayload);
+    return jwts;
   }
 
-  private async generateJwts(user: User) {
-    const accessPayload = {
-      userId: user.id,
-    };
-    const refreshPayload = {
-      userId: user.id,
-      jti: uuidv4()
-    };
+  async refresh(currentRefreshTokenPayload: RefreshTokenPayload): Promise<AuthServiceRes> {
+    try {
+      const session = await this.sessionsService.getSession(currentRefreshTokenPayload.sessionId);
+      if (!session) throw new UnauthorizedException('Session expired or invalid');
 
-    const accessToken = this.jwtService.sign(accessPayload, {
-      secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
-      expiresIn: '5m',
-    });
-    const refreshToken = this.jwtService.sign(refreshPayload, {
-      secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
-      expiresIn: '30d',
-    });
+      const newSessionId = uuidv4()
+      const updateSessionPayload: UpdateSession = {
+        userId: currentRefreshTokenPayload.userId,
+        currentSessionId: currentRefreshTokenPayload.sessionId,
+        newSessionId: newSessionId,
+      }
 
-    return { accessToken, refreshToken };
+      const refreshResult = await this.sessionsService.updateSession(updateSessionPayload);
+      if(refreshResult === 'NOT_FOUND') {
+        throw new UnauthorizedException('Session expired or invalid')
+      }
+
+      const accessToken = await this.tokensService.generateAccessToken(currentRefreshTokenPayload.userId);
+
+      if(refreshResult === 'OK') {
+
+        const refreshToken = await this.tokensService.generateRefreshToken(currentRefreshTokenPayload.userId, newSessionId); 
+        return {
+          accessToken: accessToken,
+          refreshToken: refreshToken
+        }
+      }
+
+      if(refreshResult === 'ALREADY_UPDATED') {
+        return {
+          accessToken: accessToken,
+        }
+      }
+
+      throw new InternalServerErrorException('Unexpected refresh result');
+
+    } catch (e) {
+      if(e instanceof JsonWebTokenError) {
+        throw new UnauthorizedException('Session expired or invalid');
+      }
+      throw e; 
+    }
   }
 
   private async validateUser(dto: UserLoginDto): Promise<User> {
