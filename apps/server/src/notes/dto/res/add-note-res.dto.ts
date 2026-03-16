4dd7f8e420603bccd5ff3c9567073bf0fdb71173
NikolaS <nikolaevs1006@gmail.com>
2026-03-16 12:58:45 +0300

    feat(notes): add class transformer decorators to AddNoteResDto

--- apps/server/src/notes/dto/res/add-note-res.dto.ts
+++ apps/server/src/notes/dto/res/add-note-res.dto.ts
@@ -1,16 +1,24 @@
 import { Note, NoteColors } from "@my-notes/types";
+import { Expose, Transform } from "class-transformer";
 
 export class AddNoteResDto implements Note{
-
+  @Expose()
   id: number;
-  
+
+  @Expose()
   title: string;
 
+  @Expose()
   mainText: string;
 
+  @Expose()
   colorKey: keyof NoteColors;
 
-  createdAt: string; // не решённый вопрос
-  
-  updatedAt: string; // не решенный вопрос
+  @Expose()
+  @Transform(({ value }) => value instanceof Date ? value.toISOString() : value)
+  createdAt: string; 
+
+  @Expose()
+  @Transform(({ value }) => value instanceof Date ? value.toISOString() : value)
+  updatedAt: string;
 }
