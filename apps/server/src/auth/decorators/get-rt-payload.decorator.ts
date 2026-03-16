41c96e8862fe50e1835a231cbe5d19e579439823
NikolaS <nikolaevs1006@gmail.com>
2026-03-16 18:48:26 +0300

    refactor(auth): move decorator to decorators folder

---
+++ apps/server/src/auth/decorators/get-rt-payload.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+export const GetRtPayload = createParamDecorator(
+  (data: string | null, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+
+    const payload = request.refreshTokenPayload;
+    if (!payload) {
+      return null;
+    }
+
+    return data ? payload[data] : payload;
+  }
+)
