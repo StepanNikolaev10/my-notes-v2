e7fa781cb376fea24c49e1bf6feccb038aa9742d
NikolaS <nikolaevs1006@gmail.com>
2026-03-18 19:54:05 +0300

    refactor(auth): replace JwtModule with TokensModule in AuthModule imports

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { TokensModule } from 'src/tokens/tokens.module';
 import { SessionsModule } from 'src/sessions/sessions.module';
@@ -11,10 +10,8 @@ import { SessionsModule } from 'src/sessions/sessions.module';
   providers: [AuthService],
   imports: [
     UsersModule,
-    JwtModule.register({}),
     TokensModule,
     SessionsModule
-  ],
-  exports: [JwtModule]
+  ]
 })
 export class AuthModule {}
