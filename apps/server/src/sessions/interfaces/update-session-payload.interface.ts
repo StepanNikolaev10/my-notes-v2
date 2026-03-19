1650050d2cda3bfa480669ad2bc82b04b0529284
NikolaS <nikolaevs1006@gmail.com>
2026-03-19 08:26:19 +0300

    refactor(sessions): rename interface

--- apps/server/src/sessions/interfaces/update-session-payload.interface.ts
+++ apps/server/src/sessions/interfaces/update-session-payload.interface.ts
@@ -1,7 +1,7 @@
 import type { SessionPayload } from "./session-payload.interface";
 import type { SessionId } from '../types/session-id.type';
 
-export interface UpdateSession {
+export interface UpdateSessionPayload {
   readonly userId: SessionPayload['userId'];
   readonly currentSessionId: SessionId;
   readonly newSessionId: SessionId;
