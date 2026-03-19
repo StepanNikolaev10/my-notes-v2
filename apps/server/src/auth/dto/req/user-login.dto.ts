28934405f4e0ce06f8f926b8aab75e70d26f88ca
NikolaS <nikolaevs1006@gmail.com>
2026-03-19 12:38:13 +0300

    feat(auth): add class validator, class transformer decorators to auth dtos

--- apps/server/src/auth/dto/req/user-login.dto.ts
+++ apps/server/src/auth/dto/req/user-login.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class UserLoginDto {
+  @IsNotEmpty({ message: 'Email cannot be empty' })
+  @IsString({ message: 'Email must be a string' })
+  @IsEmail({}, { message: 'Invalid email format' })
   readonly email: string;
+
+  @IsNotEmpty({ message: 'Password cannot be empty' })
   readonly password: string;
 }
--- apps/server/src/auth/dto/res/auth-res.dto.ts
+++ apps/server/src/auth/dto/res/auth-res.dto.ts
@@ -1,3 +1,6 @@
+import { Expose } from "class-transformer";
+
 export class AuthResDto {
+  @Expose()
   readonly accessToken: string;
 }
