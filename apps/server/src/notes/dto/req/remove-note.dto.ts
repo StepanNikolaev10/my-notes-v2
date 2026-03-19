8beb485e45efd9b58b8dd52e4425380a9e11a0fe
NikolaS <nikolaevs1006@gmail.com>
2026-03-19 18:00:19 +0300

    refactor(notes): delete authorId field from RemoveNoteDto

--- apps/server/src/notes/dto/req/remove-note.dto.ts
+++ apps/server/src/notes/dto/req/remove-note.dto.ts
@@ -1,12 +1,8 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
-import type { Note, User } from '@my-notes/types';
+import type { Note } from '@my-notes/types';
 
 export class RemoveNoteDto {
   @IsNotEmpty({ message: 'Note ID cannot be empty' })
   @IsNumber({}, { message: 'Note ID must be a number' })
   readonly id: Note['id'];
-
-  @IsNotEmpty({ message: 'Author ID cannot be empty' })
-  @IsNumber({}, { message: 'Author ID must be a number' })
-  readonly authorId: User['id'];
 }
