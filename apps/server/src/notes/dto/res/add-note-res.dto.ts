566a04f0b7abd91e8fb3ba1cc5acb56c59b47c0c
NikolaS <nikolaevs1006@gmail.com>
2026-03-19 12:45:41 +0300

    feat(notes): add readonly flags in add note res dto

--- apps/server/src/notes/dto/res/add-note-res.dto.ts
+++ apps/server/src/notes/dto/res/add-note-res.dto.ts
@@ -1,24 +1,24 @@
 import { Note, NoteColors } from "@my-notes/types";
 import { Expose, Transform } from "class-transformer";
 
-export class AddNoteResDto implements Note{
+export class AddNoteResDto implements Note {
   @Expose()
-  id: number;
+  readonly id: number;
 
   @Expose()
-  title: string;
+  readonly title: string;
 
   @Expose()
-  mainText: string;
+  readonly mainText: string;
 
   @Expose()
-  colorKey: keyof NoteColors;
-
-  @Expose()
-  @Transform(({ value }) => value instanceof Date ? value.toISOString() : value)
-  createdAt: string; 
+  readonly colorKey: keyof NoteColors;
 
   @Expose()
   @Transform(({ value }) => value instanceof Date ? value.toISOString() : value)
-  updatedAt: string;
+  readonly createdAt: string; 
+
+  @Expose()
+  @Transform(({ value }) => value instanceof Date ? value.toISOString() : value)
+  readonly updatedAt: string;
 }
