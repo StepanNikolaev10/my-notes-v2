ff2608202805df0508538e6bf07ba84b886addfe
NikolaS <nikolaevs1006@gmail.com>
2026-03-20 13:04:05 +0300

    feat(server): add cookie parser middleware to main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,              // Отрезает всё, что не описано в DTO
