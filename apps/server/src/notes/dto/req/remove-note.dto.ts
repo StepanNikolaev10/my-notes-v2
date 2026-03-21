5f96cb69927c1980a9450153e0408024cc3dcf63
NikolaS <nikolaevs1006@gmail.com>
2026-03-21 11:26:13 +0300

    refactor(notes): remove unnecessary RemoveNoteDto

--- apps/server/src/notes/dto/req/remove-note.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
-import type { Note } from '@my-notes/types';
-
-export class RemoveNoteDto {
-  @IsNotEmpty({ message: 'Note ID cannot be empty' })
-  @IsNumber({}, { message: 'Note ID must be a number' })
-  readonly id: Note['id'];
-}
