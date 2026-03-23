f96ba075d982af032db6eba51f5fd049ddd900bb
NikolaS <nikolaevs1006@gmail.com>
2026-03-23 17:20:02 +0300

    feat(libs): create constants library with NOTE_COLORS as first constant

---
+++ libs/constants/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NOTE_COLORS = void 0;
+exports.NOTE_COLORS = {
+    UNCOLORED: 'transparent',
+    FIRST: 'rgba(169, 29, 36, 1)',
+    SECOND: 'rgba(208, 112, 43, 1)',
+    THIRD: 'rgba(182, 122, 12, 1)',
+    FOURTH: 'rgba(6, 127, 46, 1)',
+    FIFTH: 'rgba(40, 139, 99, 1)',
+    SIXTH: 'rgba(94, 136, 170, 1)',
+    SEVENTH: 'rgba(34, 74, 107, 1)',
+    EIGHTH: 'rgba(107, 44, 93, 1)',
+    NINETH: 'rgba(172, 97, 122, 1)',
+    TENTH: 'rgba(126, 97, 55, 1)',
+    ELEVENTH: 'rgba(49, 48, 48, 1)'
+};
