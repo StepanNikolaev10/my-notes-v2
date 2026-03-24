843d39ed0f00fff16e053c059f9797a39da1fb0b
NikolaS <nikolaevs1006@gmail.com>
2026-03-24 10:50:41 +0300

    refactor(libs): use NoteKeysColors for colorKey in types library

--- libs/types/note.interface.ts
+++ libs/types/note.interface.ts
@@ -1,9 +1,10 @@
-import type { NoteColors } from "./note-colors.interface";
+import type { NoteColorsKeys } from "./note-colors.interface";
+
 export interface Note {
   id: number;
   title: string,
   mainText: string
-  colorKey: keyof NoteColors;
+  colorKey: NoteColorsKeys;
   createdAt: Date | string; // на клиент эти данные приходят строкой из за JSON.stringify
   updatedAt: Date | string; // на клиент эти данные приходят строкой из за JSON.stringify
 }
