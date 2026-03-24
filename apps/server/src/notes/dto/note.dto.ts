1a5cefbc594e178d6c4b9240eaad6fb07734857e
NikolaS <nikolaevs1006@gmail.com>
2026-03-24 19:18:38 +0300

    fix(notes): add 2 implements fields in note dto

--- apps/server/src/notes/dto/note.dto.ts
+++ apps/server/src/notes/dto/note.dto.ts
@@ -1,10 +1,12 @@
-import { Note, NoteColorsKeys  } from "@my-notes/types";
+import type { Note, NoteColorsKeys  } from "@my-notes/types";
 
 export class NoteDto implements Note {
   readonly id: number;
   readonly title: string;
   readonly mainText: string;
   readonly colorKey: NoteColorsKeys
+  readonly isArchived: boolean;
+  readonly isTrashed: boolean;
   readonly createdAt: string;
   readonly updatedAt: string;
 }
