b010fc0643a0fa01a3f7dc3a0558e2a429802c95
NikolaS <nikolaevs1006@gmail.com>
2026-03-25 09:54:26 +0300

    feat(notes): add new notes manipulation methods in controller and service

--- apps/server/src/notes/notes.controller.ts
+++ apps/server/src/notes/notes.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Post, UseGuards } from '@nestjs/common';
 import { AddNoteDto } from './dto/req/add-note.dto';
-import { RemoveNoteDto } from './dto/req/remove-note.dto';
 import { NotesService } from './notes.service';
 import { JwtAccessAuthGuard } from 'src/auth/guards/jwt-access-auth.guard';
 import { AddNoteResDto } from './dto/res/add-note-res.dto';
 import { plainToInstance } from 'class-transformer';
 import { GetAccessTokenPayload } from 'src/auth/decorators/get-at-payload.decorator';
 import type { Tokens } from 'src/auth/types/tokens.interface';
-import type { NotesServiceArgs } from './types/notes-service-args.interface';
+import { ArchiveNotesDto } from './dto/req/archive-notes.dto';
+import { UnarchiveNotesDto } from './dto/req/unarchive-notes.dto';
+import { TrashNotesDto } from './dto/req/trash-notes.dto';
+import { RestoreTrashedNotesDto } from './dto/req/restore-trashed-notes.dto';
+import { DeleteNotesDto } from './dto/req/delete-notes.dto';
+import { UpdateNotesColorDto } from './dto/req/update-notes-color.dto';
+import { UpdateNoteContentDto } from './dto/req/update-note-content.dto';
+import { UpdateNotesColorResDto } from './dto/res/update-notes-color.dto';
+import { UpdateNoteContentResDto } from './dto/res/update-note-content-res.dto';
 
 @Controller('notes')
 export class NotesController {
@@ -23,28 +30,73 @@ export class NotesController {
     @GetAccessTokenPayload() accessTokenPayload: Tokens['accessTokenPayload']
   ): Promise<AddNoteResDto> { // передав дженерик промису мы говорим что данный метод на resolve ...
   // ... вернёт тип переданный в этом дженерике.
-    const addNoteArgs: NotesServiceArgs['addNote'] = {
-      ...dto,
-      authorId: accessTokenPayload.userId
-    }
-    const createdNote = await this.notesService.addNote(addNoteArgs); // await ловит resolve, а код после является then, если ошибка то это throw и catch
-
-    return plainToInstance(AddNoteResDto, createdNote); // это синхронная функция, она ждёт значение, а не промис, поэтому выше нужен await
+    const serviceResult = await this.notesService.addNote({ ...dto, authorId: accessTokenPayload.userId }); // await ловит resolve, а код после является then, если ошибка то это throw и catch
+    return plainToInstance(AddNoteResDto, serviceResult); // это синхронная функция, она ждёт значение, а не промис, поэтому выше нужен await
   }
 
-  
-  
-  // @Post('/remove')
-  // @UseGuards(JwtAccessAuthGuard)
-  // removeNote(
-  //   @Body() dto: RemoveNoteDto,
-  //   @GetAccessTokenPayload() accessTokenPayload: AccessTokenPayload
-  // ): Promise<void> {
-  //   const removeNotePayload = {
-  //     ...dto,
-  //     userId: accessTokenPayload.userId
-  //   }
-  //   return this.notesService.removeNote(removeNotePayload);
-  // }
+  @Post('/archive')
+  @UseGuards(JwtAccessAuthGuard)
+  archiveNotes(
+    @Body() dto: ArchiveNotesDto,
+    @GetAccessTokenPayload() accessTokenPayload: Tokens['accessTokenPayload']
+  ): void {
+    this.notesService.archiveNotes({ ...dto, authorId: accessTokenPayload.userId });
+  }
 
+  @Post('/unarchive')
+  @UseGuards(JwtAccessAuthGuard)
+  unarchiveNotes(
+    @Body() dto: UnarchiveNotesDto,
+    @GetAccessTokenPayload() accessTokenPayload: Tokens['accessTokenPayload']
+  ): void {
+    this.notesService.unarchiveNotes({ ...dto, authorId: accessTokenPayload.userId });
+  }
+
+  @Post('/trash')
+  @UseGuards(JwtAccessAuthGuard)
+  trashNotes(
+    @Body() dto: TrashNotesDto,
+    @GetAccessTokenPayload() accessTokenPayload: Tokens['accessTokenPayload']
+  ): void {
+    this.notesService.trashNotes({ ...dto, authorId: accessTokenPayload.userId });
+  }
+
+  @Post('/restore-trashed')
+  @UseGuards(JwtAccessAuthGuard)
+  restoreTrashedNotes(
+    @Body() dto: RestoreTrashedNotesDto,
+    @GetAccessTokenPayload() accessTokenPayload: Tokens['accessTokenPayload']
+  ): void {
+    this.notesService.restoreTrashedNotes({ ...dto, authorId: accessTokenPayload.userId });
+  }
+
+  @Delete('/delete')
+  @UseGuards(JwtAccessAuthGuard)
+  deleteNotes(
+    @Body() dto: DeleteNotesDto,
+    @GetAccessTokenPayload() accessTokenPayload: Tokens['accessTokenPayload']
+  ): void {
+    this.notesService.deleteNotes({ ...dto, authorId: accessTokenPayload.userId });
+  }
+
+  @Post('/update-color')
+  @UseGuards(JwtAccessAuthGuard)
+  async updateNotesColor(
+    @Body() dto: UpdateNotesColorDto,
+    @GetAccessTokenPayload() accessTokenPayload: Tokens['accessTokenPayload']
+  ): Promise<UpdateNotesColorResDto> {
+    const serviceResult = await this.notesService.updateNotesColor({ ...dto, authorId: accessTokenPayload.userId });
+    return plainToInstance(UpdateNotesColorResDto, serviceResult);
+  }
+
+  @Post('/update-content')
+  @UseGuards(JwtAccessAuthGuard)
+  async UpdateNoteContent(
+    @Body() dto: UpdateNoteContentDto,
+    @GetAccessTokenPayload() accessTokenPayload: Tokens['accessTokenPayload']
+  ): Promise<UpdateNoteContentResDto> {
+    const serviceResult = await this.notesService.updateNoteContent({ ...dto, authorId: accessTokenPayload.userId });
+    return plainToInstance(UpdateNoteContentResDto, serviceResult);
+  }
+  
 }
--- apps/server/src/notes/notes.service.ts
+++ apps/server/src/notes/notes.service.ts
@@ -1,11 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { AddNoteDto } from './dto/req/add-note.dto';
-import { RemoveNoteDto } from './dto/req/remove-note.dto';
 import { NoteEntity } from './note.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import type { NotesServiceArgs } from './types/notes-service-args.interface';
-import type { NotesServiceResults } from './types/notes-service-results.interface';
+import { In, Repository } from 'typeorm';
+import { AddNoteArgs, ArchiveNotesArgs, DeleteNotesArgs, RestoreTrashedNotesArgs, TrashNotesArgs, UnarchiveNotesArgs, UpdateNoteContentArgs, UpdateNotesColorArgs } from './types/notes-service-args.interfaces';
+import { AddNoteResult, UpdateNoteContentResult, UpdateNotesColorResult } from './types/notes-service-results.interfaces';
+
 
 @Injectable()
 export class NotesService {
@@ -15,21 +14,87 @@ export class NotesService {
   private noteRepository: Repository<NoteEntity>  // значения падает в userRepository,  ...
   // ...но для удобной работы используется тип Repository с добавлением методов автокомлитом и дженерик с типами сущности Note
 
-  async addNote(args: NotesServiceArgs['addNote']): Promise<NotesServiceResults['addNote']> { // за счёт noteRepository создаётся экземпляр класса сущности Note, значения из dto подставляются в нужные места.
+  async addNote(args: AddNoteArgs): Promise<AddNoteResult> { // за счёт noteRepository создаётся экземпляр класса сущности Note, значения из dto подставляются в нужные места.
     const note = this.noteRepository.create(args);
     await this.noteRepository.save(note);
-    return note;
+    return { createdAt: note.createdAt, updatedAt: note.updatedAt }
   }
 
-  // async removeNote(data: RemoveNoteDto): Promise<void> {
-  //   const result = await this.noteRepository.delete({
-  //     id: data.id,
-  //     authorId: dto.authorId
-  //   });
-    
-  //   if (result.affected === 0) {
-  //     throw new NotFoundException(`Note with ID ${dto.id} not found or access denied`);
-  //   }
-  // }
+  async archiveNotes(args: ArchiveNotesArgs): Promise<void> {
+    await this.noteRepository.update(
+      { id: In(args.noteIds), authorId: args.authorId, isArchived: false, isTrashed: false },
+      { isArchived: true }
+    );
+  }
+
+  async unarchiveNotes(args: UnarchiveNotesArgs): Promise<void> {
+    await this.noteRepository.update(
+      { id: In(args.noteIds), authorId: args.authorId, isArchived: true, isTrashed: false },
+      { isArchived: false }
+    );
+  }
+
+  async trashNotes(args: TrashNotesArgs): Promise<void> {
+    await this.noteRepository.update(
+      { id: In(args.noteIds), authorId: args.authorId, isTrashed: false },
+      { isArchived: false, isTrashed: true }
+    );
+  }
+
+  async restoreTrashedNotes(args: RestoreTrashedNotesArgs): Promise<void> {
+    await this.noteRepository.update(
+      { id: In(args.noteIds), authorId: args.authorId, isTrashed: true },
+      { isTrashed: false }
+    );
+  }
+
+  async deleteNotes(args: DeleteNotesArgs): Promise<void> {
+    await this.noteRepository.delete(
+      { id: In(args.noteIds), authorId: args.authorId, isTrashed: true }
+    );
+  }
+
+  async updateNotesColor(args: UpdateNotesColorArgs): Promise<UpdateNotesColorResult> {
+    await this.noteRepository.update(
+      { id: In(args.noteIds), authorId: args.authorId, isTrashed: false },
+      { colorKey: args.updatedColorKey }
+    )
+
+    const updatedNotes = await this.noteRepository.find({
+      where: {
+        id: In(args.noteIds),
+        authorId: args.authorId,
+        isTrashed: false
+      },
+      select: ['id', 'updatedAt']
+    });
+
+    return {
+      updatedNotesData: updatedNotes.map(note => ({
+        noteId: note.id,
+        updatedAt: note.updatedAt
+      }))
+    }
+  }
+
+  async updateNoteContent(args: UpdateNoteContentArgs): Promise<UpdateNoteContentResult> {
+    await this.noteRepository.update(
+      { id: args.noteId, authorId: args.authorId, isTrashed: false },
+      { title: args.updatedTitle, mainText: args.updatedMainText }
+    )
+
+    const updatedNote = await this.noteRepository.findOne({
+      where: { id: args.noteId, authorId: args.authorId },
+      select: ['updatedAt']
+    })
+
+    if (!updatedNote) {
+      throw new NotFoundException('The note was not found or is not editable for you');
+    }
+
+    return {
+      updatedAt: updatedNote.updatedAt
+    }
+  }
 
 }
