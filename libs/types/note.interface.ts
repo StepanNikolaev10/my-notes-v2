6b401d9bdf3ab1b1e381c662e80e335c98e624b0
NikolaS <nikolaevs1006@gmail.com>
2026-03-25 10:18:04 +0300

    feat(libs): add positionNumber field to note interface in types library

--- libs/types/note.interface.ts
+++ libs/types/note.interface.ts
@@ -1,12 +1,13 @@
 import type { NoteColorsKeys } from "./note-colors.interface";
 
 export interface Note {
-  id: number;
-  title: string,
-  mainText: string
-  colorKey: NoteColorsKeys;
-  isArchived: boolean;
-  isTrashed: boolean;
-  createdAt: Date | string; // на клиент эти данные приходят строкой из за JSON.stringify
-  updatedAt: Date | string; // на клиент эти данные приходят строкой из за JSON.stringify
+  readonly id: number;
+  readonly title: string,
+  readonly mainText: string
+  readonly colorKey: NoteColorsKeys;
+  readonly positionNumber: number;
+  readonly isArchived: boolean;
+  readonly isTrashed: boolean;
+  readonly createdAt: Date | string; // на клиент эти данные приходят строкой из за JSON.stringify
+  readonly updatedAt: Date | string; // на клиент эти данные приходят строкой из за JSON.stringify
 }
